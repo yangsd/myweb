6733bcde5835feb9eb843a6ce5127ee9321c0d14
sdyang <1289668604@qq.com>
2018-01-17 17:37:24 +0800

    整合beetl

--- src/main/java/com/yiyi/auth/controller/LoginController.java
+++ src/main/java/com/yiyi/auth/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.yiyi.auth.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author sdyang
@@ -12,4 +15,17 @@ import org.springframework.stereotype.Controller;
 public class LoginController {
 
     private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+
+    /**
+     * 登录页面
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = {"","/login"}, method = {RequestMethod.GET})
+    public String login(Model model) {
+        return "login.html";
+    }
+
 }
